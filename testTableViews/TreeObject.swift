7303705c447db76e5e55129aebb6703b068f94a9
George <54204157+jorjyb0i@users.noreply.github.com>
2021-04-07 16:40:02 +0500

    Initial commit

---
+++ testTableViews/TreeObject.swift
@@ -0,0 +1,82 @@
+//
+//  StrangeEntity.swift
+//  testTableViews
+//
+//  Created by jorjyb0i on 26.03.2021.
+//
+
+import Foundation
+import UIKit
+
+class TreeObject {
+    
+    lazy var children: [TreeObject] = createChildren()
+    
+    var pNum: Int
+    var title: String
+    let isCollection: Bool = Bool.random()
+    
+    init(num: Int) {
+        pNum = num
+        title = "\(pNum): " + UUID().uuidString
+    }
+    
+    var isHidden: Bool = true
+    var isActive: Bool = false
+    
+    private func createChildren() -> [TreeObject] {
+        var newArray: [TreeObject] = []
+        let numberOfElements = Int.random(in: 2...5)
+        
+        if numberOfElements != 0 {
+            for _ in 0..<numberOfElements {
+                newArray.append(TreeObject(num: self.pNum + 1))
+            }
+        }
+        return newArray
+    }
+    
+    func setHidden() {
+        isHidden = !isHidden
+    }
+    
+    func setActive() {
+        isActive = !isActive
+    }
+    
+    func numberOfObjectsBelowLine() -> Int {
+        var count: Int = 0
+        
+        for child in children {
+            if !child.isHidden && child.isActive {
+                count += 1 + child.numberOfObjectsBelowLine()
+            } else if !child.isHidden {
+                count += 1
+            } else {
+                return 0
+            }
+        }
+        return count
+    }
+    
+    func findElement(withIndex index: Int) -> TreeObject? {
+        let numberOfChilndren = children.count
+        var objectsToIgnore = 0
+        
+        for currentIndex in 0..<numberOfChilndren {
+            
+            if currentIndex == index - objectsToIgnore {
+                return children[currentIndex]
+            }
+            
+            let numberOfObjectsBelowLine = children[currentIndex].numberOfObjectsBelowLine()
+            
+            if numberOfObjectsBelowLine + currentIndex < index  {
+                objectsToIgnore += numberOfObjectsBelowLine
+            } else {
+                return children[currentIndex].findElement(withIndex: index - currentIndex - objectsToIgnore - 1)
+            }
+        }
+        return nil
+    }
+}
--- testTableViews/ViewController.swift
+++ testTableViews/ViewController.swift
@@ -9,11 +9,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let tableView = UITableView()
+    let identifier = "mainTable"
+    
+    var numberOfRowsBelowTheLne = 0
+    
+    let treeObject = TreeObject(num: 0)
+    var arrayOfCells: [TreeObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        treeObject.setActive()
+        treeObject.setHidden()
+        for child in treeObject.children {
+            child.setHidden()
+        }
+        
+        arrayOfCells = treeObject.children
+        
+        setupTableView(table: tableView, identifier: identifier)
+        activateConstraints()
     }
-
-
+    
+    func setupTableView(table: UITableView, identifier: String) {
+        view.addSubview(table)
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.delegate = self
+        table.dataSource = self
+        table.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+    }
+    
+    func activateConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
 
+// -MARK: TableView Data Source & Delegate
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let selectedParent = treeObject.findElement(withIndex: indexPath.row) {
+            tableView.beginUpdates()
+            if !selectedParent.isActive {
+                var increment = indexPath.row + 1
+                for child in selectedParent.children {
+                    
+                    arrayOfCells.insert(child, at: increment)
+                    tableView.insertRows(at: [IndexPath.init(row: increment, section: 0)], with: .automatic)
+                    
+                    increment += 1
+                }
+                selectedParent.setActive()
+                for child in selectedParent.children {
+                    child.setHidden()
+                }
+                
+            } else {
+                
+            }
+            tableView.endUpdates()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfCells.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        cell.textLabel?.text = arrayOfCells[indexPath.row].title
+        return cell
+    }
+}
